cba61057e3e2e1154fea367635e8d691be14f9c2
Yehonatan <yehonatanrefael@gmail.com>
2024-11-22 14:54:36 +0200

    ready to production

--- server/src/hooks/link.hook.ts
+++ server/src/hooks/link.hook.ts
@@ -1,25 +1,20 @@
 import { NextFunction } from 'express';
 
-const PREFIX_URL: string = 'https://thelosttreasures.net/?';
+const PREFIX_URL = 'https://thelosttreasures.net/';
 
 const addPrefix = (link: string) => {
     if (link.startsWith(PREFIX_URL)) {
         return link;
     }
-    return PREFIX_URL + link;
+    return PREFIX_URL + '?' + link;
 };
-
 const linkHook = (schema) => {
     schema.pre('save', function (next: NextFunction) {
         this.link = addPrefix(this.link);
         next();
     });
-
     schema.pre('findOneAndUpdate', function (next: NextFunction) {
-        const update = this.getUpdate();
-        if (update.link) {
-            update.link = addPrefix(update.link);
-        }
+        this._update.link = addPrefix(this._update.link);
         next();
     });
 };
