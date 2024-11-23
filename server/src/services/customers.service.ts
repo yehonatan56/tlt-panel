4017c5051076ebefef1fd5332260454feb76cc23
Yehonatan <yehonatanrefael@gmail.com>
2024-11-23 20:16:54 +0200

    backend

--- server/src/services/customers.service.ts
+++ server/src/services/customers.service.ts
@@ -1,5 +1,12 @@
 import customerModel from '../models/customers.model';
 import { ICustomer } from '../interfaces/customer.interface';
+
+const getCustomersServiceHandler = async (page: number): Promise<ICustomer[]> => {
+    return (await customerModel
+        .find({})
+        .skip(page * 8)
+        .limit(8)) as unknown as ICustomer[];
+};
 const addCustomerServiceHandler = async (customer: ICustomer): Promise<string> => {
     const customerDoc = new customerModel(customer);
 
@@ -7,4 +14,4 @@ const addCustomerServiceHandler = async (customer: ICustomer): Promise<string> =
     return customerDoc._id as unknown as string;
 };
 
-export { addCustomerServiceHandler };
+export { getCustomersServiceHandler, addCustomerServiceHandler };
