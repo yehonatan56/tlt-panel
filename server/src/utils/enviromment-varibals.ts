fe0ae269f22b1403b734268d54f4df90732303f0
Yehonatan <yehonatanrefael@gmail.com>
2024-12-13 12:20:32 +0200

    not safe76

--- server/src/utils/enviromment-varibals.ts
+++ server/src/utils/enviromment-varibals.ts
@@ -1,15 +1,11 @@
-import dotenv from 'dotenv';
-import { ENV_PATH } from '../paths';
-
-dotenv.config({ path: ENV_PATH });
-
-const CONNECTION_STRING = process.env.CONNECTION_STRING || 'mongodb://localhost:27017/test';
-const NODE_ENV = process.env.NODE_ENV || 'development';
-const CLOUDINARY_CLOUD_NAME = process.env.CLOUDINARY_CLOUD_NAME;
-const CLOUDINARY_API_KEY = process.env.CLOUDINARY_API_KEY;
-const CLOUDINARY_API_SECRET = process.env.CLOUDINARY_API_SECRET;
-const SECRET = process.env.SECRET || 'secret';
-const WHATSAPP_GROUP_ID = process.env.WHATSAPP_GROUP_ID;
+const CONNECTION_STRING = 'mongodb+srv://root:A12345a@cluster0.ivjfmh6.mongodb.net/tlt';
+const NODE_ENV = 'production';
+const CLOUDINARY_CLOUD_NAME = 'dk5irlhb0';
+const CLOUDINARY_API_KEY = '575444824168269';
+const CLOUDINARY_API_SECRET = 'B04tsDT0JuZfjmRGuNkFek5iXw';
+const SECRET =
+    'fBmxyy9SJ1VZFqqMgXYghkErk156ymsrIQt3tRhLkS6GD1G2ZAF2fYBgcbK9t45GJgTfVdJ9b9Ht0FFGzsjq3ogHhar3nQcRcR6gBxWh7HhZnW2eswe01waXQEhtXR6badOpB0gWns6h rLAF99zoCvKcnDF0TU';
+const WHATSAPP_GROUP_ID = '1203633749548055953ag.us';
 
 export {
     CONNECTION_STRING,
--- server/src/utils/jest.ts
+++ server/src/utils/jest.ts
@@ -1,10 +1,7 @@
-import dotenv from 'dotenv';
 import connectDB from '../config/db';
-import { ENV_PATH } from '../paths';
 import usersModel from '../models/users.model';
 
 jest.setTimeout(30000);
-dotenv.config({ path: ENV_PATH });
 
 beforeAll(async () => {
     await connectDB().then(async () => {
