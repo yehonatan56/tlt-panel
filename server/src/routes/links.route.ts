abb19d3acbe8148aa717ea8b24e88112b8a0743e
Yehonatan <yehonatanrefael@gmail.com>
2025-01-01 13:10:29 +0200

    no whatsapp

--- server/src/routes/links.route.ts
+++ server/src/routes/links.route.ts
@@ -13,8 +13,6 @@ import {
 import { addCustomerCtrl } from '../controllers/customers.cotroller';
 import { loggerMW } from '../middlewars/logger.middleware';
 import upload from '../utils/cloudinary';
-import { whatsappMW } from '../middlewars/whatsapp.middleware';
-
 const router = express.Router();
 
 router.use(loggerMW);
@@ -22,7 +20,7 @@ router.get('/', isAuthorizedUserMW, getLinksCtrl);
 router.get('/pages', isAuthorizedUserMW, getPagesCtrl);
 router.get('/highest', isAuthorizedUserMW, getHighestPurchasesCtrl);
 router.post('/', isAuthorizedUserMW, createLinkCtrl);
-router.post('/purchase', addCustomerCtrl, whatsappMW, purchaseCtrl);
+router.post('/purchase', addCustomerCtrl, purchaseCtrl);
 
 router.post('/upload', isAuthorizedUserMW, upload.single('image'), uploadCtrl);
 router.put('/:id', isAuthorizedUserMW, editLinkCtrl);
