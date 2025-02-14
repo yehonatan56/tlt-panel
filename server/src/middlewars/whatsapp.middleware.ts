ccba08158ef3aac51dcdc4070a6a667c730306f1
Yehonatan <yehonatanrefael@gmail.com>
2025-02-14 13:04:26 +0200

    Implement login error handling

--- server/src/middlewars/whatsapp.middleware.ts
+++ server/src/middlewars/whatsapp.middleware.ts
@@ -1,10 +1,7 @@
-import { TOKEN, WHATSAPP_GROUP_ID, WHATSAPP_GROUP_ID_COMMUNITY } from '../utils/enviromment-varibals';
+import { TOKEN, WHATSAPP_GROUP_ID } from '../utils/enviromment-varibals';
 import convertPhone from '../functions/convertPhone';
-import { createVerify } from 'node:crypto';
-
 export const whatsappMW = async (req, _res, next) => {
-    const message = `
-    new purchase:
+    const message = ` new purchase:
     name: ${req.body.firstName} ${req.body.lastName}
     phone: ${req.body.phone}
     email: ${req.body.email}
@@ -13,46 +10,30 @@ export const whatsappMW = async (req, _res, next) => {
     pickup method: ${req.body.pickupMethod}
     `;
 
-    const messageToCustomer = `
-    היי ${req.body.firstName} ${req.body.lastName},
+    const messageToCustomer = ` היי ${req.body.firstName} ${req.body.lastName},
     קיבלנו את ההזמנה שלך, נטפל בה בהקדם האפשרי.
-    צירפנו אותך לקבוצת הוואטסאפ שלנו, כדי לקבל עדכונים והצעות מיוחדות.
-    אם אתה רוצה להסיר את עצמך מהקבוצה, תוכל לעשות זאת בכל עת.
+    יש לנו קבוצת וואטסאפ שקטה ללקוחות, תוכל להצטרף ולקבל עדכונים ומבצעים מיוחדים.
+    להצטרפות לחץ על הקישור הבא:
+https://chat.whatsapp.com/LEtTIFqBZQtE8JCMUJEBfg
+    תודה רבה,
+    לפניות  נוספות ניתן לפנות אלינו בטלפון: 055-315-1517
     `;
-    // fetch('https://gate.whapi.cloud/messages/text', {
-    //     method: 'POST',
-    //     headers: {
-    //         Accept: 'application/json',
-    //         Authorization: 'Bearer ' + TOKEN,
-    //         'Content-Type': 'application/json',
-    //     },
-    //     body: JSON.stringify({
-    //         typing_time: 0,
-    //         to: WHATSAPP_GROUP_ID,
-    //         body: message,
-    //     }),
-    // })
-    //     .then((response) => response.json())
-    //     .then((data) => console.log('Success:', data))
-    //     .catch((error) => console.error('Error:', error));
-
-    fetch(`https://gate.whapi.cloud/groups/${WHATSAPP_GROUP_ID_COMMUNITY}/participants`, {
+    fetch('https://gate.whapi.cloud/messages/text', {
         method: 'POST',
         headers: {
             Accept: 'application/json',
             Authorization: 'Bearer ' + TOKEN,
             'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ participants: [convertPhone(req.body.phone)] }), // Adjust slicing if needed
+        body: JSON.stringify({
+            typing_time: 0,
+            to: WHATSAPP_GROUP_ID,
+            body: message,
+        }),
     })
-        .then((response) => {
-            if (!response.ok) {
-                throw new Error(`HTTP error! status: ${response.status}`);
-            }
-            return response.json();
-        })
+        .then((response) => response.json())
         .then((data) => console.log('Success:', data))
-        .catch((error) => console.log('Error:', error));
+        .catch((error) => console.error('Error:', error));
 
     fetch('https://gate.whapi.cloud/messages/text', {
         method: 'POST',
